0d96e1de1427fa4ce8755f146305de642d3222c0
Dharun Vignesh G <22suca026@mannarcollege.ac.in>
2025-08-12 10:44:57 +0530

    Add files via upload

--- school/server/Helpers/SendEmail.cjs
+++ school/server/Helpers/SendEmail.cjs
@@ -21,7 +21,7 @@ const sendOTP = async(to) => {
         }
     })
     const mailOptions = {
-        from: 'XXXXXXXXXXXXXX',
+        from: 'ganeshgowri1985@gmail.com',
         text: `3 Minutes OTP for Schools : ${otp} ! Hurry up!`,
         to: to,
     }
@@ -62,7 +62,7 @@ const sendForgotLink = async(email,id,loc) => {
         }
     })
     const mailOptions = {
-        from: 'XXXXXXXXXXXXXXXXX',
+        from: 'ganeshgowri1985@gmail.com',
         text: `http://localhost:5173/${loc}/${id.toString().replaceAll('/',')')}/${email.toString().replaceAll('/',')')}`,
         to: email,
     }
@@ -76,6 +76,5 @@ const sendForgotLink = async(email,id,loc) => {
         console.log(`http://localhost:5173/${loc}/${id.toString().replaceAll('/',')')}/${email.toString().replaceAll('/',')')}`)
     }
 }
-
 
 module.exports = {sendOTP, validateOTP, generateID, sendForgotLink}
